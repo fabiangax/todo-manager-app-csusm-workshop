61a336b4a5f79808d2c03a4a9659e9e2e9c93270
Fabian Gaxiola <fabianeduardo.gaxiola@thermofisher.com>
2024-04-10 18:24:16 -0700

    Removed vscode config folder and files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Task } from './models/task.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ToDoManagerApp';
+  newTask!: Task;
+  tasks: Array<Task> = [];
+
+  constructor() {
+    this.newTask = { title: '', description: '', deadline: '', status: 'uncompleted' };
+  }
+
+  addTask() {
+    console.log(this.newTask);
+    this.tasks.push(this.newTask);
+    console.log(this.tasks);
+    this.newTask = { title: '', description: '', deadline: '', status: 'uncompleted' };
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter((t) => t !== task);
+  }
+
+  completeTask(task: Task) {
+    task.status = 'completed';
+  }
+
+  uncompleteTask(task: Task) {
+    task.status = 'uncompleted';
+  }
 }
